72beb46b81bdc6a3093016677c373148002cec03
root <root@DESKTOP-7RJJVM7.localdomain>
2018-01-19 01:32:37 -0500

    Google Auth Complete

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
 var express = require('express');
+const authRoutes = require('./routes/auth-routes')
+const profileRoutes = require('./routes/profile-routes')
 var bodyParser = require('body-parser');
 var path = require('path');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+
+app.use(cookieSession({
+	maxAge: 24*60*60*1000,
+	keys:[keys.session.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect to database
+mongoose.connect(keys.mongodb.dbURI, ()=>{
+	console.log('connected to mongodb')
+});
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static(__dirname +'/public'))
 
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
 var first_name = 'Rohan'
 
-
 app.get('/', function(req, res){
-	res.render('index', {
-		name: first_name
-	})
+	res.render('index')
 });
 
-app.get('/about.html', function(req, res){
-	res.render('about')
+/*
+app.get('/welcome', function(req, res){
+	res.render('welcome', {
+		name: first_name
+	})
+});*/
+
+app.get('/quiz', function(req, res){
+	res.render('quiz')
 });
 
 app.listen(3000, function(){
